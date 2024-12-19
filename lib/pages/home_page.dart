ffb1e2d160e5595ffa19042753e1afed75d54cc9
LINDEX171 <lindex171@gmail.com>
2024-12-19 16:53:07 +0000

    serch and drawer done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,61 +8,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        toolbarHeight: 70,
-        title: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.3),
-                blurRadius: 10,
-                offset: const Offset(0, 4),
-              ),
-            ],
-          ),
-          child: Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.menu, color: Colors.black),
-                onPressed: () {
-                },
-              ),
-
-              // Espacement entre le menu et le champ de recherche
-              const SizedBox(width: 10),
-
-              // Barre de recherche
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Rechercher dans les messages",
-                    hintStyle: TextStyle(color: Colors.grey[600]),
-                    border: InputBorder.none,
+      key: _scaffoldKey,
+      backgroundColor: Colors.white,
+      drawer: Drawer(),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Container(
+          height: 50,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.white, boxShadow: [
+            BoxShadow(
+              offset: Offset(4, 4),
+              blurRadius: 10,
+              color: Colors.grey.withOpacity(0.4),
+            )
+          ]),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    _scaffoldKey.currentState?.openDrawer();
+                  },
+                  icon: Icon(Icons.menu),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "Rechercher parmis les messages",
+                        border:
+                            UnderlineInputBorder(borderSide: BorderSide.none)),
                   ),
                 ),
-              ),
-
-              // Avatar circulaire
-              const SizedBox(width: 10),
-              CircleAvatar(
-                radius: 20,
-                backgroundImage: NetworkImage("https://th.bing.com/th/id/OIP.8xC-Y-eFY84Set5-4ubV5AHaE7?rs=1&pid=ImgDetMain"), // Remplacez par votre image
-              ),
-            ],
+                SizedBox(
+                  width: 10,
+                ),
+                CircleAvatar(
+                  backgroundColor: Colors.orange,
+                )
+              ],
+            ),
           ),
         ),
-      ),
-      body: Center(
-        child: Text("Contenu principal"),
-      ),
+      )),
     );
   }
 }
-
